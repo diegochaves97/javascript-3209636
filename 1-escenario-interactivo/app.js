367757281ea5e3d9c14e4de2cc90ae9f5aeb5c2f
DCH1997 <dchavesgarcia90@gmail.com>
2025-10-21 15:25:41 -0500

    Escenario interactivo funcional

--- 1-escenario-interactivo/app.js
+++ 1-escenario-interactivo/app.js
@@ -1,2 +1,26 @@
-const monedas = document.querySelectorAll(".coin")
-const score = document.querySelectorAll(".puntaje")
+const monedas = document.querySelectorAll(".coins")
+const score = document.querySelector(".puntaje")
+let marcador = 0
+
+console.log(monedas) /*no debe dar null*/
+console.log(score)
+
+monedas .forEach(item => {
+item.addEventListener("click", () => {
+    item.classList.add("saltomoneda")
+marcador++ 
+score.textContent = `X ${marcador}`
+})
+
+
+
+
+
+
+item.addEventListener("animationend", () => {
+    item.style,display.add = "none"
+}, {once: true})
+
+
+
+})
