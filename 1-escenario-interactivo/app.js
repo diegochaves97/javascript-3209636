0fe28d0f147fc37015354769b2a910f530814efb
DCH1997 <dchavesgarcia90@gmail.com>
2025-10-24 20:57:48 -0500

    tercer escenario e interactividad añadidos

--- 1-escenario-interactivo/app.js
+++ 1-escenario-interactivo/app.js
@@ -24,3 +24,47 @@ item.addEventListener("animationend", () => {
 
 
 })
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+const shyguys = document.querySelectorAll(".sg")
+const score2 = document.querySelector(".puntaje2")
+let marcador2 = 0
+
+
+shyguys .forEach(item => {
+item.addEventListener("click", () => {
+    item.classList.add("saltoshyg")
+marcador2++ 
+score2.textContent = `X ${marcador2}`
+})
+
+item.addEventListener("animationend", () => {
+    item.style,display.add = "none"
+}, {once: true})
+
+
+
+})
+
+/*////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+const goombas = document.querySelectorAll(".gb")
+const score3 = document.querySelector(".puntaje3")
+let marcador3 = 0
+
+
+goombas .forEach(item => {
+item.addEventListener("click", () => {
+    item.classList.add("caidagoomba")
+marcador3++ 
+score3.textContent = `X ${marcador3}`
+})
+
+item.addEventListener("animationend", () => {
+    item.style,display.add = "none"
+}, {once: true})
+
+
+
+})
