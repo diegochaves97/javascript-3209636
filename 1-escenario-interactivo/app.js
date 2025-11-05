f054ff090935b85e58597a9d000e3c20b68b5d53
DCH1997 <dchavesgarcia90@gmail.com>
2025-11-05 15:43:32 -0500

    sonidos agregados

--- 1-escenario-interactivo/app.js
+++ 1-escenario-interactivo/app.js
@@ -1,6 +1,8 @@
 const monedas = document.querySelectorAll(".coins")
 const score = document.querySelector(".puntaje")
+const sonidoMoneda = new Audio("./sonidos/mario-coin-sound-effect.mp3");
 let marcador = 0
+
 
 console.log(monedas) /*no debe dar null*/
 console.log(score)
@@ -10,15 +12,13 @@ item.addEventListener("click", () => {
     item.classList.add("saltomoneda")
 marcador++ 
 score.textContent = `X ${marcador}`
-})
 
-
-
-
+    sonidoMoneda.play();
+    })
 
 
 item.addEventListener("animationend", () => {
-    item.style,display.add = "none"
+    item.style.display.add = "none"
 }, {once: true})
 
 
@@ -29,6 +29,7 @@ item.addEventListener("animationend", () => {
 
 const shyguys = document.querySelectorAll(".sg")
 const score2 = document.querySelector(".puntaje2")
+const sonidoshyg = new Audio("./sonidos/smw_stomp.wav");
 let marcador2 = 0
 
 
@@ -37,7 +38,10 @@ item.addEventListener("click", () => {
     item.classList.add("saltoshyg")
 marcador2++ 
 score2.textContent = `X ${marcador2}`
-})
+
+    sonidoshyg.play();
+    })
+
 
 item.addEventListener("animationend", () => {
     item.style,display.add = "none"
@@ -49,25 +53,36 @@ item.addEventListener("animationend", () => {
 
 /*////////////////////////////////////////////////////////////////////////////////////////////////*/
 
-const goombas = document.querySelectorAll(".gb")
-const score3 = document.querySelector(".puntaje3")
-let marcador3 = 0
+const goombas = document.querySelectorAll(".gb");
+const score3 = document.querySelector(".puntaje3");
+let marcador3 = 0;
+const sonidoGoomba = new Audio("./sonidos/smw_pipe.wav");
+const pipesAmarillas = document.querySelectorAll(".pipey1, .pipey2, .pipey3");
 
+pipesAmarillas.forEach((pipe, index) => {
+  pipe.addEventListener("click", () => {
+    const goomba = goombas[index]; 
 
-goombas .forEach(item => {
-item.addEventListener("click", () => {
-    item.classList.add("caidagoomba")
-marcador3++ 
-score3.textContent = `X ${marcador3}`
-})
+    if (!goomba) return; 
+ 
+    if (!goomba.classList.contains("caidagoomba")) {
+      goomba.classList.add("caidagoomba");
+      marcador3++;
+      score3.textContent = `X ${marcador3}`;
 
-item.addEventListener("animationend", () => {
-    item.style,display.add = "none"
-}, {once: true})
-
-
-
-})
+     
+      sonidoGoomba.currentTime = 0;
+      sonidoGoomba.play();
+      goomba.addEventListener(
+        "animationend",
+        () => {
+          goomba.style.display = "none";
+        },
+        { once: true }
+      );
+    }
+  });
+});
 
 /*/////////////////////////////////////////CARRUSEL////////////////////////////////////////////*/
 
