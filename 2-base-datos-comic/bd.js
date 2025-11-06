7eeb5bd6fa13b04968221d71434651fd89fbd749
DCH1997 <dchavesgarcia90@gmail.com>
2025-11-06 15:20:29 -0500

    Base de datos, comic

---
+++ 2-base-datos-comic/bd.js
@@ -0,0 +1,41 @@
+let comic = {
+  "nombrecomic": "El tiempo enterrado",
+  "sinopsis": "Metro City",
+  "year": 2016,
+  "autores": "Diego Alonso Chaves Garcia",
+  "personajes": ["p1", "p2", "p3"]
+  "genero": "...",
+  "portada": "./image",
+  "escenas": [ 
+    
+    {
+          "id"= 1,
+          "nombre": "moleculaman"
+          "personajes": ["p1", "p2", "p3"]
+          "imagen": "./"
+    },
+
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
