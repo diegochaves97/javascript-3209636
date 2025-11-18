040c7ddbcaeda51d33805a67783669f052bb61cd
DCH1997 <dchavesgarcia90@gmail.com>
2025-11-18 15:44:12 -0500

    estructura basica - escenas

--- 2-base-datos-comic/bd.js
+++ 2-base-datos-comic/bd.js
@@ -2,16 +2,48 @@ let comic = {
   "nombrecomic": "El tiempo enterrado",
   "sinopsis": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nihil veritatis reiciendis id. Perspiciatis dolorum debitis placeat quos, asperiores fugit ea repellat unde excepturi  uaerat ad necessitatibus vero ipsum rerum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatqem tempore magnam sunt, mollitia ad repellat? Eius adipisci voluptatum, porro distinctio ipsa",
   "year": 2016,
-  "autores": "Diego Alonso Chaves Garcia",
+  "autores": ["Diego Alonso Chaves", "Juan Jose"],
   "personajes": ["James Watt", "Mineros", "Eloy", "Ninove"],
   "genero": ["fantasia", "accion", "ficcion", "aventura"],
-  "portada": "./image",
+  "portada": "./img/Portada.png",
+  "producido": "Narramentum",
+  "status": "completo",
+   
+  "personajes":[
+    
+    {
+      "nombre": "James Watt",
+      "imagen": "",
+    },
+
+ {
+      "nombre": "Mineros",
+      "imagen": "",
+    },
+
+     {
+      "nombre": "Eloy",
+      "imagen": "",
+    },
+
+     {
+      "nombre": "Ninove",
+      "imagen": "",
+    },
+
+  ],
+
+
+
+
   "escenas": [  
+
     
     {
           "id": 1,
           "nombre": "moleculaman",
           "personajes": ["p1", "p2", "p3"],
+          "descripcion": "",
           "imagen": "./",
     },
 
--- 2-base-datos-comic/index.js
+++ 2-base-datos-comic/index.js
@@ -1,7 +1,48 @@
 const sinopsis1 = document.querySelector (".containersinopsis")
+const informacion = document.querySelector (".info-comic")
+const castdepersonajes = document.querySelector(".castdepersonajes")
+const listaescenas = document.querySelector(".lista-escenas")
 
 sinopsis1.innerHTML =` 
  <h2 class="sinopsis"> Sinopsis </h2>
- <p class="sinopsistexto"> ${comic.sinopsis} </p>
-               
+ <p class="sinopsistex              <li> Mineros </li>
+                    <li> Ninove </li>
+                </li>
+            </div>
+
+to"> ${comic.sinopsis} </p>           
 ` 
+
+informacion.innerHTML = `
+ <aside class="info-comic">
+            <div class="portada"> <img src="${comic.portada}"> </div>
+            <section class="seccion2">
+                <div class="informacion">
+                    <ul class="informacion1">
+                         <li> Genero: ${comic.genero} </li>
+                        <li> Fecha: ${comic.year} </li>
+                        <li> Estado: ${comic.status}</li>
+                        <li> Producido: ${comic.producido}</li>
+                        <li> Creadores: ${comic.autores}</li>
+                        <li> Pagina Oficial: <a href="www.Narramentum.com"> Narramentum Website </a> </li>
+                    </ul>
+
+                </div>
+            </section>
+        </aside>
+
+` 
+
+castdepersonajes.innerhtml =  ` 
+ <div class="castdepersonajes">
+                <h1 class="Personajes"> Personajes - Cast </h1>
+                <ul class="Personajeslista">
+                    <li> Elloy </li>
+                    <li> James Watt </li>
+       
+`
+
+listacapitulos.innerHTML = ` 
+
+
+`  
